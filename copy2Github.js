d84084ac2b6f58d7f1bb5c3263e2b74c78dcc654
huasenjio <184820911@qq.com>
2023-04-15 16:42:36 +0800

    feat：优化图标和迁移脚本

--- copy2Github.js
+++ copy2Github.js
@@ -2,14 +2,14 @@
  * @Autor: huasenjio
  * @Date: 2023-04-15 02:12:10
  * @LastEditors: huasenjio
- * @LastEditTime: 2023-04-15 02:14:27
+ * @LastEditTime: 2023-04-15 16:41:33
  * @Description: 拷贝项目文件到Github开源仓库
  */
 const { copyFiles } = require("./copyProject.js");
 
 copyFiles(
-  "/Users/huasenjio/Documents/iCollections/编程环境/github/huasenjio-compose",
-  "/Users/huasenjio/Documents/iCollections/编程环境/gitee/huasenjio-compose"
+  "/Users/huasenjio/Documents/iCollections/编程环境/github/huasen-compose",
+  "/Users/huasenjio/Documents/iCollections/编程环境/github/huasenjio-compose"
 )
   .then(() => console.log("Files copied successfully."))
   .catch((err) => console.error(err));
