b4e0a8d5de6d3cb7c0fa88012b4264202c41ea1b
huasenjio <184820911@qq.com>
2023-05-14 13:25:07 +0800

    add：后台服务新增日志多删接口

--- huasen-server/controller/record.controller.js
+++ huasen-server/controller/record.controller.js
@@ -2,7 +2,7 @@
  * @Autor: huasenjio
  * @Date: 2022-10-29 12:07:17
  * @LastEditors: huasenjio
- * @LastEditTime: 2023-03-12 10:59:12
+ * @LastEditTime: 2023-05-14 12:09:29
  * @Description: 日志控制器
  */
 
@@ -64,8 +64,25 @@ function remove(req, res, next) {
   );
 }
 
+function removeMany(req, res, next) {
+  let { _ids } = req.huasenParams;
+  req.epWorking(
+    [
+      {
+        schemaName: 'Record',
+        methodName: 'deleteMany',
+        payloads: [{ _id: { $in: _ids } }],
+      },
+    ],
+    result => {
+      global.huasen.responseData(res, result, 'SUCCESS', '删除站点成功', false);
+    },
+  );
+}
+
 module.exports = {
   add,
   remove,
   findAllByPage,
+  removeMany,
 };
